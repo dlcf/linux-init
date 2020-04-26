7a83cb12e76850fd23ea7162c7377a72865a372e
dlcf <15130091299@139.com>
2020-04-26 16:11:28 +0800

    update

---
+++ baoding/02-install-tools.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#error and exit = set -e
+#set -o errexit
+#color
+source /etc/init.d/functions
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+toolsName="lsof lrzsz telnet traceroute net-tools bash-completion vim yum-utils yum-plugin-versionlock python3-pip git sshpass mtr conntrack-tools libseccomp libtool-ltdl device-mapper-persistent-data lvm2 conntrack ipvsadm ipset jq sysstat iotop iftop htop glances tree"
+
+function deal(){
+  yum install $toolsName -y > /dev/null 2>&1
+}
+
+
+function main(){
+  action "$0" deal
+}
+
+main $*
---
+++ baoding/03-install-docker.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#error and exit = set -e
+#set -o errexit
+#color
+source /etc/init.d/functions
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+function createConfig(){
+mkdir -p /etc/docker/
+cat>/etc/docker/daemon.json<<\EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "registry-mirrors": [
+    "https://2ph0ua9a.mirror.aliyuncs.com",
+    "https://hub-mirror.c.163.com",
+    "https://registry.docker-cn.com",
+    "https://docker.mirrors.ustc.edu.cn"
+  ],
+  "data-root":"/opt/docker",
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m",
+    "max-file": "3"
+  }
+}
+EOF
+}
+
+function deal(){
+  if [ ! -f /etc/docker/daemon.json ] ;then
+     createConfig
+  fi
+  if [ ! -f /etc/yum.repos.d/docker-ce.repo ] ;then
+     curl -sL http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo -o /etc/yum.repos.d/docker-ce.repo
+  fi
+  export docker_version=19.03.8
+  version=$(yum list docker-ce.x86_64 --showduplicates|sort -r|grep ${docker_version}|awk '{print $2}')
+  yum -y install --setopt=obsoletes=0 docker-ce-${docker_version} docker-ce-selinux-${docker_version} docker-compose > /dev/null 2>&1
+  systemctl enable --now docker > /dev/null 2>&1
+}
+
+
+function main(){
+  action "$0" deal
+  # yum -y install yum-plugin-versionlock
+  # yum versionlock docker-ce
+  # yum versionlock delete docker-ce
+  # yum versionlock clear
+}
+
+main $*
